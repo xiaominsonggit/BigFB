bc2371871a140f9eee2718c4bd5d4c2b15542467
Chuan Yang <splendidbird@gmail.com>
2016-10-01 20:38:04 -0700

    initial upload

---
+++ ColumnReduce.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import h5py
+import matplotlib.pyplot as plt
+
+h5f = h5py.File('Generated_train.hdf5','r')
+b = h5f['Chunk_1'][:]
+bb = np.array(b)
+print(len(b))
+countnon = np.zeros(970)
+for chunknum in range(1,19):
+	datasetname = 'Chunk_'+ str(chunknum)
+	currentdataset = h5f[datasetname][:]
+	currentdataset_np = np.array(currentdataset)
+	countnon = countnon + pd.DataFrame(currentdataset_np).isnull().sum()
+	print(countnon)
+#	break
+countnon.plot()
+	
+		
+
+
+
+
+
+
+h5f.close()
+
